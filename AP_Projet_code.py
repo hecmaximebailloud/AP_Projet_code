34b4cfe402afa35e3d3d722f7a4cb4c310dbb6b1
hecmaximebailloud <maxime.bailloud@unil.ch>
2024-05-19 00:07:32 +0200

    Update AP_Projet_code.py

--- AP_Projet_code.py
+++ AP_Projet_code.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.feature_selection import RFE
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
 
 st.title("Streamlit App with Code Display")
 
